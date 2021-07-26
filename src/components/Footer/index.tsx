fdf605dcabaababbec2508b7baa05ae703f311c2
Thalles Ian <thallesyam@users.=.github.com>
2021-07-25 21:49:44 -0300

    feat: add link to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -21,7 +21,11 @@ export const Footer = ({ isIcon = false }: FooterProps): JSX.Element => {
             />
           </li>
           <li>
-            <Social IconComponent={'Github'} link="/" isTransparent />
+            <Social
+              IconComponent={'Github'}
+              link="https://github.com/thallesyam"
+              isTransparent
+            />
           </li>
         </ul>
       )}
